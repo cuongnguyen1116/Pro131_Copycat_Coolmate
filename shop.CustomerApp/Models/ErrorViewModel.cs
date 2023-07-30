f1f4d56d5869ea5d70207f73d74b093327b8532e
cuongnguyen1116 <cuongnguyenpm1116@gmail.com>
2023-07-30 07:50:24 +0700

    xóa customer app cũ (chỉ có phần giao diện chưa hoàn thành của Phạm Đình Khải, chưa có 1 luồng, chức năng nào được triển khai

--- shop.CustomerApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace shop.CustomerApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
