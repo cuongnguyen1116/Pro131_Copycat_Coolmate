cbb8e630937a43ad23093c3c8ec723bd667a20ad
cuongnguyen1116 <cuongnguyenpm1116@gmail.com>
2023-07-30 07:51:37 +0700

    tạo mới customer app code lại từ đầu

---
+++ shop.CustomerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shop.CustomerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
